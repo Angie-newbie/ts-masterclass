ddac3914af4ae3a09d77e34aa9ad78a63c69a906
Angie <angel890817@hotmail.com>
2025-06-16 14:16:45 +1000

    union type

--- core-types.js
+++ core-types.js
@@ -9,3 +9,29 @@ let numbers = [1, 2, 3, 4, 5];
 // let user = ['Matt', 52, true] // [name, age, isAdmin]
 let user;
 user = ['Matt', 52, true];
+// let user: [string, number, boolean?] // ? = optional
+// user = ['Matt', 52]
+// inline object as a datatype
+let car;
+car = { make: 'MG', model: 'HS', year: 2021, electric: false };
+console.log(car);
+let person1 = {
+    id: 1,
+    firstName: 'Ada',
+    lastName: 'Lovelace',
+    greet: function () {
+        return `Hi, I am ${this.firstName} ${this.lastName}!`;
+    }
+};
+console.log(person1.greet());
+//Union type
+function printId(id) {
+    if (typeof id === 'string') { // type guard
+        console.log(`your ID is ${id.toUpperCase()}`);
+    }
+    else {
+        console.log(`your ID is ${id}`);
+    }
+}
+printId(101);
+printId('abc123');
--- core-types.ts
+++ core-types.ts
@@ -16,4 +16,45 @@ let numbers: number[] = [1, 2, 3, 4, 5]
 let user: [string, number, boolean]
 user = ['Matt', 52, true]
 
+// let user: [string, number, boolean?] // ? = optional
+// user = ['Matt', 52]
 
+
+// inline object as a datatype
+let car: {make: string, model:string, year: number, electric? : boolean }
+car = {make: 'MG', model: 'HS', year: 2021, electric: false}
+console.log(car)
+
+interface Person {
+    id: number
+    firstName: string
+    lastName: string
+    age?: number
+    greet(): string // method signature
+}
+
+let person1: Person ={
+    id : 1,
+    firstName:'Ada',
+    lastName:'Lovelace',
+    greet: function() {
+        return `Hi, I am ${this.firstName} ${this.lastName}!`
+    }
+}
+
+console.log(person1.greet())
+
+
+//Union type
+function printId(id: number | string): void{ //Pipe (|) creates a unio type
+    if (typeof id === 'string'){ // type guard
+        console.log(`your ID is ${id.toUpperCase()}`)
+    } else {
+        console.log(`your ID is ${id}`)
+    }
+    
+}
+
+printId(101)
+
+printId('abc123')
